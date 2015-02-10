e6be611964a6890147aad7264bbb92cefb4f391b
Raoulinc <raoulwoerkom@gmail.com>
2015-02-10 12:14:01 +0100

    Void written

--- ​XNAInvaders/_​XNAInvaders/Invader.cs
+++ ​XNAInvaders/_​XNAInvaders/Invader.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using Microsoft.Xna.Framework;
+
 namespace __XNAInvaders
 {
     class Invader
     {
+        public Vector2 position;
+        public Vector2 velocity;
+
+        public Invader() { }
+
+        void Init()
+        { }
+
+        void update()
+        { }
+        void Draw() { }
     }
 }
